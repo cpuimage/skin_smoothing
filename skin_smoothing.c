b4a5c7d0ae2f635e638250d3078ff036401bd1a7
cpuimage <gaozhihan@vip.qq.com>
2023-06-26 12:51:36 +0800

    fix of variance error

--- skin_smoothing.c
+++ skin_smoothing.c
@@ -221,6 +221,10 @@ void getOffsetPos(int *offsetPos, int length, int left, int right, int step) {
     }
 }
 
+int Abs(int v) {
+    return (v ^ (v >> 31)) - (v >> 31);
+}
+
 void skinDenoise(unsigned char *input, unsigned char *output, int width, int height, int channels, int radius,
                  int smoothingLevel) {
     if ((input == NULL) || (output == NULL)) return;
@@ -293,7 +297,7 @@ void skinDenoise(unsigned char *input, unsigned char *output, int width, int hei
             const int diff = mean - scanInLine[c];
             const int edge = ClampToByte(diff);
             const int masked_edge = (edge * scanInLine[c] + (256 - edge) * mean) >> 8;
-            const int var = max(prevPowerSum[c] - mean * prevSum[c], 0) / windowSize;
+            const int var = Abs(prevPowerSum[c] - mean * prevSum[c]) / windowSize;
             const int out = masked_edge - diff * var / (var + smoothLut[scanInLine[c]]);
             scanOutLine[c] = ClampToByte(out);
         }
@@ -309,7 +313,7 @@ void skinDenoise(unsigned char *input, unsigned char *output, int width, int hei
                 const int diff = mean - scanInLine[c];
                 const int edge = ClampToByte(diff);
                 const int masked_edge = (edge * scanInLine[c] + (256 - edge) * mean) >> 8;
-                const int var = max(prevPowerSum[c] - mean * prevSum[c], 0) / windowSize;
+                const int var = Abs(prevPowerSum[c] - mean * prevSum[c]) / windowSize;
                 const int out = masked_edge - diff * var / (var + smoothLut[scanInLine[c]]);
                 scanOutLine[c] = ClampToByte(out);
             }
